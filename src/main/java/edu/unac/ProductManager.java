0cf87c186f53f691ba2573aab8a4e7200a5d4d96
andresdiaz153 <andresf.diaza@unac.edu.co>
2025-06-05 16:02:48 -0500

    añado test

--- src/main/java/edu/unac/ProductManager.java
+++ src/main/java/edu/unac/ProductManager.java
@@ -34,11 +34,11 @@ public class ProductManager {
         return new ArrayList<>(products.values());
     }
 
-    public List<Product> getProductsByPrice(double min, double max) {
+    public List<Product> getProductsByPriceRange(double min, double max) {
         List<Product> result = new ArrayList<>();
-        for (Product p : products.values()) {
-            if (p.getPrice() >= min && p.getPrice() <= max) {
-                result.add(p);
+        for (Product product : products.values()) {
+            if (product.getPrice() >= min && product.getPrice() <= max) {
+                result.add(product);
             }
         }
         return result;
